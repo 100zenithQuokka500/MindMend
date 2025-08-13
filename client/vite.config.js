1cc923ce16184d31f46866182ab4a86495e1f104
Himangi Tripathy <123282807+H1mang1@users.noreply.github.com>
2025-08-13 19:30:19 +0530

    Update Vite configuration to change API proxy target and enable changeOrigin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-       '/api':'https://mindmend-backend-1y9v.onrender.com'    },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mindmend-backend-19y9.onrender.com/',
+        changeOrigin: true,
+        secure: false, 
+      },
+    },
   },
   plugins: [react()],
-})
+});
