fd24d0a44305d07b4766031a50ec305ee857329c
Himangi Tripathy <123282807+H1mang1@users.noreply.github.com>
2025-08-26 18:15:52 +0530

    Update Vite configuration to change API proxy target from localhost to production URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'https://mindmend-backend-19y9.onrender.com',
         changeOrigin: true,
         secure: false, 
       },
